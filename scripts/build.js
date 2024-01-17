8bce5715834a982c7b81f05f1d5da4b6a08e3260
Amy Yan <ayanamydev@gmail.com>
2024-01-17 14:20:55 +1100

    fix: commit hash is now derived from files within `.git` in `build.js`

--- scripts/build.js
+++ scripts/build.js
@@ -16,6 +16,7 @@ async function main(argv = process.argv) {
   const projectRoot = path.join(__dirname, '..');
   const buildPath = path.join(projectRoot, 'build');
   const distPath = path.join(projectRoot, 'dist');
+  const gitPath = path.join(projectRoot, '.git');
   await fs.promises.rm(distPath, {
     recursive: true,
     force: true,
@@ -30,11 +31,24 @@ async function main(argv = process.argv) {
     shell: platform === 'win32' ? true : false,
   });
   // This collects the build metadata and adds it to the build folder so that dynamic imports to it will resolve correctly.
+  let gitHead = process.env.COMMIT_HASH;
+  if (gitHead == null) {
+    gitHead = await fs.promises.readFile(path.join(gitPath, 'HEAD'), 'utf-8');
+    if (gitHead.startsWith('ref: ')) {
+      const refPath = gitHead.slice(5).trim();
+      gitHead = await fs.promises.readFile(
+        path.join(gitPath, refPath),
+        'utf-8',
+      );
+    }
+  }
   const buildJSON = {
     versionMetadata: {
-      cliAgentCommitHash: process.env.COMMIT_HASH,
+      cliAgentCommitHash: gitHead,
     },
   };
+  console.error('Writing build metadata (build.json):');
+  console.error(buildJSON);
   await fs.promises.writeFile(
     path.join(buildPath, 'build.json'),
     JSON.stringify(buildJSON, null, 2),
