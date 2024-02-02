f2fe1e27dba5c4125a524c26ee6ec0f7323215f4
Brynley Llewellyn-Roux <blynblack@gmail.com>
2024-02-02 12:21:11 +1100

    feat: fixed overlay compatibility

--- default.nix
+++ default.nix
@@ -1,5 +1,6 @@
 { npmDepsHash ? ""
 , commitHash ? null
+, gitDir ? null
 , callPackage
 , buildNpmPackage
 }:
@@ -18,7 +19,7 @@ in
       version = utils.packageVersion;
       src = utils.src;
       COMMIT_HASH = commitHash;
-      GIT_DIR = utils.dotGit;
+      GIT_DIR = gitDir;
       # Filter out things kept by `src`, these were needed for building
       # but not needed for subsequent usage of the store path
       postInstall = ''
