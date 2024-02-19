89baafbf2ae87383e123893abd3981294d0a5931
Roger Qiu <roger.qiu@polyhack.io>
2024-02-20 06:10:28 +1100

    fix: `npmDepsHash.js` should be using `execFileSync` with a better error message

--- scripts/npmDepsHash.js
+++ scripts/npmDepsHash.js
@@ -7,15 +7,19 @@ const childProcess = require('child_process');
 async function main(_argv = process.argv) {
   const projectRoot = path.join(__dirname, '..');
   try {
-    const output = childProcess.execSync(
-      'prefetch-npm-deps ./package-lock.json',
+    const hash = childProcess.execFileSync(
+      'prefetch-npm-deps',
+      ['./package-lock.json'],
+      {
+        windowsHide: true,
+        encoding: 'utf-8',
+      },
     );
-    const hash = output.toString();
     await fs.promises.writeFile(path.join(projectRoot, 'npmDepsHash'), hash);
   } catch (e) {
     // eslint-disable-next-line no-console
     console.error(
-      `Warning: prefetch failed with (${e.message}). if npmDepsHash was not updated`,
+      `Failed to run \`prefetch-npm-deps\` failed with (${e.message}) - this can cause builds to fail in Nix!`,
     );
   }
 }
