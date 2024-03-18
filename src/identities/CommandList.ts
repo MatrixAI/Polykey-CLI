3fda9b451aa951ce1d89d57837e63be674d7faa2
Amy Yan <ayanamydev@gmail.com>
2024-03-18 11:34:34 +1100

    fix: `identities/CommandList` now more closely follows RPC response
    fix: whitespace on `identities/CommandList`
    
    fix: `test/identities/trustUntrustList.test.ts`

--- src/identities/CommandList.ts
+++ src/identities/CommandList.ts
@@ -1,4 +1,5 @@
 import type PolykeyClient from 'polykey/dist/PolykeyClient';
+import type { GestaltMessage } from 'polykey/dist/client/types';
 import CommandPolykey from '../CommandPolykey';
 import * as binOptions from '../utils/options';
 import * as binUtils from '../utils';
@@ -42,72 +43,70 @@ class CommandList extends CommandPolykey {
           },
           logger: this.logger.getChild(PolykeyClient.name),
         });
-        const gestalts = await binUtils.retryAuthentication(async (auth) => {
-          const gestalts: Array<any> = [];
-          const stream = await pkClient.rpcClient.methods.gestaltsGestaltList({
-            metadata: auth,
-          });
-          for await (const gestaltMessage of stream) {
-            const gestalt = gestaltMessage.gestalt;
-            const newGestalt: any = {
-              permissions: [],
-              nodes: [],
-              identities: [],
-            };
-            for (const node of Object.keys(gestalt.nodes)) {
-              const nodeInfo = gestalt.nodes[node];
-              newGestalt.nodes.push({ nodeId: nodeInfo.nodeId });
-            }
-            for (const identity of Object.keys(gestalt.identities)) {
-              const identityInfo = gestalt.identities[identity];
-              newGestalt.identities.push({
-                providerId: identityInfo.providerId,
-                identityId: identityInfo.identityId,
+        const gestaltMessages = await binUtils.retryAuthentication(
+          async (auth) => {
+            const gestaltMessages: Array<
+              GestaltMessage & { gestalt: { actionsList: Array<string> } }
+            > = [];
+            const stream = await pkClient.rpcClient.methods.gestaltsGestaltList(
+              {
+                metadata: auth,
+              },
+            );
+            for await (const gestaltMessage of stream) {
+              // Getting the permissions for the gestalt.
+              const actionsMessage = await binUtils.retryAuthentication(
+                (auth) =>
+                  pkClient.rpcClient.methods.gestaltsActionsGetByNode({
+                    metadata: auth,
+                    nodeIdEncoded: Object.values(
+                      gestaltMessage.gestalt.nodes,
+                    )[0].nodeId,
+                  }),
+                auth,
+              );
+              const actionsList = actionsMessage.actionsList;
+              gestaltMessages.push({
+                gestalt: {
+                  ...gestaltMessage.gestalt,
+                  actionsList,
+                },
               });
             }
-            // Getting the permissions for the gestalt.
-            const actionsMessage = await binUtils.retryAuthentication(
-              (auth) =>
-                pkClient.rpcClient.methods.gestaltsActionsGetByNode({
-                  metadata: auth,
-                  nodeIdEncoded: newGestalt.nodes[0].nodeId,
-                }),
-              auth,
-            );
-            const actionList = actionsMessage.actionsList;
-            if (actionList.length === 0) newGestalt.permissions = null;
-            else newGestalt.permissions = actionList;
-            gestalts.push(newGestalt);
-          }
-          return gestalts;
-        }, auth);
+            return gestaltMessages;
+          },
+          auth,
+        );
         if (options.format === 'json') {
           process.stdout.write(
             binUtils.outputFormatter({
-              type: options.format === 'json' ? 'json' : 'list',
-              data: gestalts,
+              type: 'json',
+              data: gestaltMessages,
             }),
           );
         } else {
           // Convert to a human-readable list.
           let count = 1;
-          for (const gestalt of gestalts) {
+          for (const gestaltMessage of gestaltMessages) {
+            const gestalt = gestaltMessage.gestalt;
+            if (count !== 1) process.stdout.write('\n');
             process.stdout.write(
               binUtils.outputFormatter({
                 type: 'dict',
                 data: {
                   gestalt: count,
-                  permissions: gestalt.permissions,
+                  actionsList: gestalt.actionsList.join(','),
                 },
               }),
             );
             // Listing nodes
-            const nodeIds = gestalt.nodes.map((node) => node.nodeId);
+            const nodeIds = Object.values(gestalt.nodes).map(
+              (node) => node.nodeId as string,
+            );
             // Listing identities
-            const identities = gestalt.identities.map(
+            const identities = Object.values(gestalt.identities).map(
               (identity) => `${identity.providerId}:${identity.identityId}`,
             );
-            if (count !== 1) process.stdout.write('\n');
             process.stdout.write(
               binUtils.outputFormatter({
                 type: 'list',
