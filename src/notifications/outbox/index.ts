8335af80ff6012f64bb44de0292406838719802a
Amy Yan <ayanamydev@gmail.com>
2024-05-06 12:22:59 +1000

    fix: moved `CommandInbox` and `CommandOutbox` to `index.ts`s

--- src/notifications/outbox/index.ts
+++ src/notifications/outbox/index.ts
@@ -1,1 +1,17 @@
-export { default } from './CommandOutbox';
+import CommandClear from './CommandClear';
+import CommandRead from './CommandRead';
+import CommandRemove from './CommandRemove';
+import CommandPolykey from '../../CommandPolykey';
+
+class CommandOutbox extends CommandPolykey {
+  constructor(...args: ConstructorParameters<typeof CommandPolykey>) {
+    super(...args);
+    this.name('outbox');
+    this.description('Notifications Outbox Operations');
+    this.addCommand(new CommandClear(...args));
+    this.addCommand(new CommandRead(...args));
+    this.addCommand(new CommandRemove(...args));
+  }
+}
+
+export default CommandOutbox;
