f9e8b46611c1b2b263b203bd6aa5e4a8f278ebbd
Brian Botha <brian.botha@matrix.ai>
2025-04-17 19:37:11 +1000

    fix: potential fix for packaged executable

--- shims/require-shim.mjs
+++ shims/require-shim.mjs
@@ -4,5 +4,10 @@
  * Some libraries that support ESM still use `require` which is causing problems in esbuild.
  */
 import { createRequire } from 'node:module';
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
 // eslint-disable-next-line no-global-assign
 require = createRequire(import.meta.url);
+let __dirname = path.dirname(fileURLToPath(import.meta.url));
+export { __dirname };
+
